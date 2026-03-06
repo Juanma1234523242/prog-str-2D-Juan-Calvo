baf28cea24496c1aee101e13cbd01c463d087efd
Juanma1234523242 <20253ds110@utez.edu.mx>
2026-03-06 07:36:13 -0600

    cambio

--- demoListView/src/main/java/com/example/demolistview/controllers/AppController.java
+++ demoListView/src/main/java/com/example/demolistview/controllers/AppController.java
@@ -1,10 +1,31 @@
 package com.example.demolistview.controllers;
 
+import com.example.demolistview.services.PersonService;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
+import javafx.collections.ObservableList;
+
+import java.io.IOException;
+import java.util.List;
 
 public class AppController {
     @FXML
-    private ListView ListView;
+    private ListView <String>ListView;
+    @FXML
+    private Label lblMsg;
 
+
+    @FXML
+    private final ObservableList<String> data = FXCollections.checkedObservabeArrayList();
+
+    private PersonService service = new PersonService();
+     private void loadFromFile(){
+         try{
+             List<String> items =service.loadDataforList();
+             data.setAll(Items);
+         }catch (IOException e);
+     }
 }
