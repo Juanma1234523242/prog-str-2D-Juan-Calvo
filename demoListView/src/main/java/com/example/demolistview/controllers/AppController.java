f997d399b6076223237f7bfe1cd9e6b073940d64
Juanma1234523242 <20253ds110@utez.edu.mx>
2026-03-06 07:22:52 -0600

    juanma

---
+++ demoListView/src/main/java/com/example/demolistview/controllers/AppController.java
@@ -0,0 +1,10 @@
+package com.example.demolistview.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class AppController {
+    @FXML
+    private ListView ListView;
+
+}
